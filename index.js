d446bcdd9ddc25b42e21d63cb3270cb0d7b9e539
Sasha <sashla1713@gmail.com>
2023-05-19 16:58:29 +0300

    Updated to make it universal

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const fs = require('fs');
 const readline = require('readline');
-const url = require("url")
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -21,7 +20,7 @@ const readTemplateFile = (templatePath, dataPath) => {
 };
 
 const readDataFile = (template, dataPath) => {
-    fs.readFile(dataPath, "utf8", (err, data) => {
+    fs.readFile(dataPath, 'utf8', (err, data) => {
         if (err) {
             console.error(`Error reading data file: ${err.message}`);
             askForDataPath(template);
@@ -37,57 +36,72 @@ const readDataFile = (template, dataPath) => {
             return;
         }
 
-        const skillListHTML = jsonData.skills.map(
-            (skill) => `<li>${skill}</li>`
-        ).join(" ");
-
-        const rendered = template
-            .replace(/{{(\w+)}}/g, (match, p1) => {
-                return jsonData[p1] || "";
-            })
-            .replace("{{skills}}", skillListHTML);
+        const rendered = generateRenderedHTML(template, jsonData);
 
         http.createServer((req, res) => {
-            if (req.url === "/style.css") {
-                fs.readFile("./style.css", "utf8", (err, css) => {
+            if (req.url === '/style.css') {
+                fs.readFile('./style.css', 'utf8', (err, css) => {
                     if (err) {
                         console.error(`Error reading CSS file: ${err.message}`);
-                        res.writeHead(404, { "Content-Type": "text/plain" });
-                        res.write("404 Not Found");
+                        res.writeHead(404, { 'Content-Type': 'text/plain' });
+                        res.write('404 Not Found');
                         res.end();
                         return;
                     }
-                    res.writeHead(200, { "Content-Type": "text/css" });
+                    res.writeHead(200, { 'Content-Type': 'text/css' });
                     res.write(css);
                     res.end();
-                    });
+                });
             } else {
-                res.writeHead(200, { "Content-Type": "text/html" });
+                res.writeHead(200, { 'Content-Type': 'text/html' });
                 res.write(rendered);
                 res.end();
-                }
+            }
         }).listen(3000, () => {
-            console.log("Check the result on http://localhost:3000");
+            console.log('Check the result on http://localhost:3000');
             rl.close();
         });
     });
 };
 
+const generateRenderedHTML = (template, jsonData) => {
+    let rendered = template;
+    let dataHTML = '';
+
+    for (const key in jsonData) {
+        const value = jsonData[key];
+        let valueHTML = '';
+        if (Array.isArray(value)) {
+            valueHTML = '<ul>';
+            value.forEach(item => {
+                valueHTML += `<li>${item}</li>`;
+            });
+            valueHTML += '</ul>';
+        } else {
+            valueHTML = value;
+        }
+        dataHTML += `<p><strong>${key}:</strong> <span>${valueHTML}</span></p>`;
+    }
+
+    rendered = rendered.replace('{{data}}', `<div class="content">${dataHTML}</div>`);
+
+    return rendered;
+};
 
 const askForTemplatePath = (dataPath) => {
-    rl.question('Enter the correct path to the template file (recommend "index.html"): ', templatePath => {
+    rl.question('Enter the correct path to the template file (recommend "index.html"): ', (templatePath) => {
         readTemplateFile(templatePath, dataPath);
     });
 };
 
 const askForDataPath = (template) => {
-    rl.question('Enter the correct path to the data file (recommend "data.json"): ', dataPath => {
+    rl.question('Enter the correct path to the data file (recommend "data.json"): ', (dataPath) => {
         readDataFile(template, dataPath);
     });
 };
 
-rl.question('Enter the path to the template file (recommend "index.html"): ', templatePath => {
-    rl.question('Enter the path to the data file (recommend "data.json"): ', dataPath => {
+rl.question('Enter the path to the template file (recommend "index.html"): ', (templatePath) => {
+    rl.question('Enter the path to the data file (recommend "data.json"): ', (dataPath) => {
         readTemplateFile(templatePath, dataPath);
     });
 });
